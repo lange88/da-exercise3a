a4fbfe0ee7cafce4f55ad326a8d5f5e6e70861ee
Jeroen Vrijenhoef <jeroen@vrijenhoef.net>
2017-12-08 11:35:13 +0100

    implemented simple security manager tests

--- src/main/java/DA_BenOr.java
+++ src/main/java/DA_BenOr.java
@@ -1,24 +1,79 @@
 import interfaces.DA_BenOr_RMI;
 import interfaces.Message;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
 
 public class DA_BenOr extends UnicastRemoteObject implements DA_BenOr_RMI, Runnable {
     private int id;
-    private int[] processes;
+    private int[] processIds;
+    private int[] remoteProcessIds;
+    private String remoteHost;
 
-    DA_BenOr(int id, int[] processes) throws RemoteException {
+    DA_BenOr(int id, int[] processIds, int[] remoteProcessIds, String remoteHost) throws RemoteException {
         this.id = id;
-        this.processes = processes;
+        this.processIds = processIds;
+        this.remoteProcessIds = remoteProcessIds;
+        this.remoteHost = remoteHost;
     }
+
+    private void broadCast(String message) {
+        // connect to local processes
+        for (int processId : processIds) {
+            String name = "rmi://localhost/DA_BenOr_" + processId;
+            try {
+                DA_BenOr_RMI o = (DA_BenOr_RMI) java.rmi.Naming.lookup(name);
+                o.receive(message);
+            } catch (NotBoundException e1) {
+                System.out.println("NotBoundException while sending message for name: " + name);
+                e1.printStackTrace();
+            } catch (MalformedURLException e1) {
+                System.out.println("MalformedURLException while sending message for name: " + name);
+                e1.printStackTrace();
+            } catch (RemoteException e1) {
+                System.out.println("RemoteException while sending message for name: " + name);
+                e1.printStackTrace();
+            }
+        }
+
+        // connect to remote processes
+        for (int processId : remoteProcessIds) {
+            String name = "rmi://" + remoteHost + "/DA_BenOr_" + processId;
+            try {
+                DA_BenOr_RMI o = (DA_BenOr_RMI) java.rmi.Naming.lookup(name);
+                o.receive(message);
+            } catch (NotBoundException e1) {
+                System.out.println("NotBoundException while sending message for name: " + name);
+                e1.printStackTrace();
+            } catch (MalformedURLException e1) {
+                System.out.println("MalformedURLException while sending message for name: " + name);
+                e1.printStackTrace();
+            } catch (RemoteException e1) {
+                System.out.println("RemoteException while sending message for name: " + name);
+                e1.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void run() {
-
+        while(true) {
+            try {
+                Thread.sleep(new Random().nextInt(5000));
+                broadCast("woopwoop" + id);
+            } catch (InterruptedException e1) {
+                System.out.println("InterruptedException occurred while running thread.");
+                e1.printStackTrace();
+                return;
+            }
+        }
     }
 
     @Override
-    public void receive(Message m) throws RemoteException {
-
+    public void receive(String m) throws RemoteException {
+        System.out.println(m);
     }
 }
--- src/main/java/DA_BenOr_main.java
+++ src/main/java/DA_BenOr_main.java
@@ -3,7 +3,6 @@ import joptsimple.OptionSet;
 
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.util.Arrays;
 
@@ -12,28 +11,9 @@ import java.util.Arrays;
  */
 public class DA_BenOr_main {
     public static void main(String... args) {
-        OptionParser parser = new OptionParser();
-
-        parser.accepts("t", "Total number of processes in DS")
-                .withRequiredArg().ofType(Integer.class);
-        parser.accepts("i", "ID of this process")
-                .withRequiredArg().ofType(String.class);
-        parser.accepts("o", "IDs of other processes in DS")
-                .withRequiredArg().ofType(String.class);
-
-        OptionSet options = parser.parse(args);
-
-        if (!options.has("i") || !options.has("t") || !options.has("o")) {
-            return;
-        }
-
-        // parse the options
-        int totalProcesses;
-        totalProcesses = (Integer) options.valueOf("t");
-        int id = (Integer) options.valueOf("i");
-        String ids = (String) options.valueOf("o");
-        int[] processIds = Arrays.stream(ids.split(" "))
-                .map(String::trim).mapToInt(Integer::parseInt).toArray();
+        int processIds[] = {1, 2, 3, 4, 5};
+        int remoteProcessIds[] = {6, 7, 8, 9, 10};
+        String remoteHost = "10.0.0.2";
 
         // Create and install a security manager because we are using multiple
         // physical machines.
@@ -49,30 +29,31 @@ public class DA_BenOr_main {
             e.printStackTrace();
         }
 
-        String name = "rmi://localhost/DA_BenOr_" + id;
-        DA_BenOr da = null;
-        try {
-            da = new DA_BenOr(id, processIds);
+        for (int i : processIds) {
+            String name = "rmi://localhost/DA_BenOr_" + i;
+            DA_BenOr da = null;
+            try {
+                da = new DA_BenOr(i, processIds, remoteProcessIds, remoteHost);
+                java.rmi.Naming.bind(name, da);
+            } catch (AlreadyBoundException e) {
+                System.out.println("AlreadyBoundException occurred while binding object with RMI name: " + name);
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                System.out.println("MalformedURLException occurred while binding object with RMI name: " + name);
+                e.printStackTrace();
+            } catch (RemoteException e) {
+                System.out.println("RemoteException occurred while binding object with RMI name: " + name);
+                e.printStackTrace();
+            }
 
-            java.rmi.Naming.bind(name, da);
-        } catch (AlreadyBoundException e) {
-            System.out.println("AlreadyBoundException occurred while binding object with RMI name: " + name);
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            System.out.println("MalformedURLException occurred while binding object with RMI name: " + name);
-            e.printStackTrace();
-        } catch (RemoteException e) {
-            System.out.println("RemoteException occurred while binding object with RMI name: " + name);
-            e.printStackTrace();
+            if (da == null) {
+                return;
+            }
+
+            // finally start the worker thread of the process
+            System.out.println("Starting process node with id=" + i
+                    + ", totalProcesses=" + processIds.length);
+            new Thread(da, "Thread for process " + i).start();
         }
-
-        if (da == null) {
-            return;
-        }
-
-        // finally start the worker thread of the process
-        System.out.println("Starting process node with id=" + id
-                + ", totalProcesses=" + totalProcesses);
-        new Thread(da, "Thread for process " + id).start();
     }
 }
